ca5ad5e0450ce8c85df8d7d0a3b2ff2c2858541b
bcecilio <35937742+bcecilio@users.noreply.github.com>
2018-09-06 20:37:20 -0700

    fixes folders

--- LGself_portraitmmp210/sketch.js
+++ LGself_portraitmmp210/sketch.js
@@ -1,9 +1,30 @@
+/*  
+    self portrait
+    v1
+    mmp210 example week 2
+*/
+
 function setup() {
-	createCanvas(440, 200);
+	createCanvas(700, 700);
 }
 
 function draw() {
-	background(900);
-	ellipse(250, 150, 100);
-    rect(90,70,200,100);
+    //default
+    stroke("white")
+    fill("red")
+    background(220);
+    //head
+        fill("black")
+        ellipse(350,350,600);
+    //eyes
+        
+        fill("white")
+        ellipse(460,280,110,200);
+        ellipse(250,280,110,200);
+        ellipse(250,280,100)
+    //mouth
+    ellipse(350, 510, 400, 310);
+    fill("black")
+    ellipse(350, 440, 300, 100);
+    line()
 }
