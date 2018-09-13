f638467940d3024e7b9db319fce4189fe041b863
lgraham93 <lgraham93@github.com>
2018-09-12 23:03:09 -0400

    Selfportrait
    final draft and color changes to first version of portrait.

--- LGself_portraitmmp210/sketch.js
+++ LGself_portraitmmp210/sketch.js
@@ -10,21 +10,37 @@ function setup() {
 
 function draw() {
     //default
-    stroke("white")
-    fill("red")
     background(220);
+    //ears
+    fill("black");
+    ellipse(160,175,250);
+    ellipse(560,175,250);
     //head
-        fill("black")
-        ellipse(350,350,600);
+        stroke("none")
+        fill("purple");
+        ellipse(350,350,590);
     //eyes
-        
-        fill("white")
+        fill("white");
+        stroke("none");
         ellipse(460,280,110,200);
         ellipse(250,280,110,200);
-        ellipse(250,280,100)
+        fill("red");
+        ellipse(260,275,100);
+        ellipse(475,275,100);
     //mouth
+    fill("white");
     ellipse(350, 510, 400, 310);
-    fill("black")
+    fill("black");
     ellipse(350, 440, 300, 100);
-    line()
+    stroke("black");
+    arc(350, 450, 20, 200, 0, PI);
+    stroke("red");
+    fill("pink");
+    arc(345, 510, 200, 100, 0, PI);
+    
+    //blood
+    fill("red");
+    arc(300, 550, 20,200, 0, PI);
+    arc(270, 540, 20,80, 0, PI);
+    
 }
