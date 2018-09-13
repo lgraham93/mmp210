86569e41cc843a44508913dc3c965bb00eb49805
bcecilio <35937742+bcecilio@users.noreply.github.com>
2018-09-13 15:41:58 -0700

    updates folder

---
+++ portraitvariables/sketch.js
@@ -0,0 +1,48 @@
+/*  
+    self portrait
+    v1
+    mmp210 example week 2
+*/
+
+function setup() {
+	createCanvas(700, 700);
+}
+    //variables
+    var eyepositionY='280';
+
+function draw() {
+    //default
+    background(220);
+    //ears
+    fill("black");
+    ellipse(160,175,250);
+    ellipse(560,175,250);
+    //head
+        stroke("none")
+        fill("purple");
+        ellipse(350,350,590);
+    //eyes
+        fill("white");
+        stroke("none");
+        ellipse(460,eyepositionY,110,200);
+        ellipse(250,eyepositionY,110,200);
+        fill("red");
+        ellipse(260,275,100);
+        ellipse(475,275,100);
+    //mouth
+    fill("white");
+    ellipse(350, 510, 400, 310);
+    fill("black");
+    ellipse(350, 440, 300, 100);
+    stroke("black");
+    arc(350, 450, 20, 200, 0, PI);
+    stroke("red");
+    fill("pink");
+    arc(345, 510, 200, 100, 0, PI);
+    
+    //blood
+    fill("red");
+    arc(300, 550, 20,200, 0, PI);
+    arc(270, 540, 20,80, 0, PI);
+    
+}
