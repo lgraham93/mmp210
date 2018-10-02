c5ee6265037d95f4bf07d8954141fc08a6a3990e
lgraham93 <lgraham@github.com>
2018-10-02 15:35:27 -0700

    meme version 0
    starting new meme project

---
+++ meme_0/sketch.js
@@ -0,0 +1,20 @@
+/* Meme version 0   
+   Lizz
+   mmp210 week 5
+*/
+
+
+function setup (){
+    createCanvas (500,400);
+    
+}
+function draw(){
+    background(87, 21, 37);
+    
+    var paragraph= "Meme Project";
+    text(paragraph, 20, 20, 200, 100);
+    fill('white');
+    textSize(40);
+	textFont('Helvetica');
+
+}
