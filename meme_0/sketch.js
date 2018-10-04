d178fb87e8f63df91a991a1cf1d2e0f86ff29f99
lgraham93 <lgraham@github.com>
2018-10-04 15:20:19 -0700

    picture added to meme0
    carlton

--- meme_0/sketch.js
+++ meme_0/sketch.js
@@ -3,18 +3,30 @@
    mmp210 week 5
 */
 
+var bman;//global
+
+function preload() {
+	carlton= loadImage("carlton.jpg");
+}
 
 function setup (){
-    createCanvas (500,400);
+    createCanvas (600,500);
     
 }
+
 function draw(){
-    background(87, 21, 37);
     
-    var paragraph= "Meme Project";
-    text(paragraph, 20, 20, 200, 100);
+    	image(carlton, 0, 0, width,height);
+    
+    var paragraph= "When your professor tells a corny joke";
+    text(paragraph, 40, 20, 300, 100);
     fill('white');
-    textSize(40);
+	textStyle(ITALIC);
+	text("But you need those 3 credits", 60, 475);
+    fill('white');
+    stroke(20);
+    textSize(30);
 	textFont('Helvetica');
+    
 
 }
