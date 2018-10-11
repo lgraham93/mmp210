2a1dc5c4e20d2c3e5710cb7caa82c58b8ade7174
lgraham93 <lgraham@github.com>
2018-10-11 15:23:15 -0700

    completed interactive meme

--- meme_1/sketch.js
+++ meme_1/sketch.js
@@ -3,48 +3,53 @@
    mmp210 week 5
 */
 
-var bman;//global
+//global
 var carlton;
+var simonc;
+var raw;
+var crying;
 
 function preload() {
-	carlton = loadImage("carlton.jpg");
+    carlton = loadImage("carlton.jpg");
+    raw = loadImage("raw.png");
+    crying=loadImage("crying.png");
+    guys=loadImage("guys.jpg");
 }
 
-function setup (){
-    createCanvas (400,400);
-    
+function setup() {
+    createCanvas(600, 600);
+
 }
 
-function draw(){
+function draw() {
     background('none');
-        // quad 1
-    if (mouseX < width/2 && mouseY < height/2) {
-        fill("blue");
-        rect(0, 0, width/2, height/2); 
-    
-    } else if (mouseX > width/2 && mouseY < height/2) {
+    // quad 1
+    if (mouseX < width / 2 && mouseY < height / 2) {
+        image(carlton, 0, 0, width, height);
+
+    } else if (mouseX > width / 2 && mouseY < height / 2) {
         //quad 2
-        fill("green");
-        rect( width/2, 0, width/2, height/2)
-    } else if ( mouseX < width/2 && mouseY > height/2) {
+        image(crying, 0, 0,width, height);
+    
+        
+    } else if (mouseX < width / 2 && mouseY > height / 2) {
         // quad 3
-        fill("orange");
-        rect( 0, width/2, width/2, height/2);
+        image(raw, 0, 0,width, height);
     } else {
         //quad 4
-        fill("purple")
-        rect(width/2, height/2, width/2,height/2);  
-    }      
+        image(guys,  0, 0,width, height);
 
-     var paragraph= "When your professor tells a corny joke";
-    text(paragraph, 40, 20, 300, 100);
+    }
+
+    var paragraph = "When your professor tells a corny joke ";
+    text(paragraph, 15, 50);
     fill('white');
-	textStyle(ITALIC);
-	text("But you need those 3 credits", 60, 475);
+    textStyle(BOLD);
+    text("But you need those 3 credits", 80, 550);
     fill('white');
     stroke(20);
     textSize(30);
-	textFont('Helvetica');
-  
+    textFont('Tahoma');
+
 
 }
