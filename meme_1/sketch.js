55dc6f4aa57aec531c88ce7b5ed6f8d8d49da05c
lgraham93 <42846843+lgraham93@users.noreply.github.com>
2018-10-18 12:53:36 -0400

    adding animation to meme

--- meme_1/sketch.js
+++ meme_1/sketch.js
@@ -8,22 +8,31 @@ var carlton;
 var simonc;
 var raw;
 var crying;
+var steveWidth=578/4;
+var steveHeight=878/4;
+var r = 0;
+var c = 10;
 
 function preload() {
     carlton = loadImage("carlton.jpg");
     raw = loadImage("raw.png");
     crying=loadImage("crying.png");
     guys=loadImage("guys.jpg");
+    steve=loadImage("steve.png");
 }
 
 function setup() {
     createCanvas(600, 600);
 
+
 }
 
+
 function draw() {
+
     background('none');
     // quad 1
+
     if (mouseX < width / 2 && mouseY < height / 2) {
         image(carlton, 0, 0, width, height);
 
@@ -41,15 +50,27 @@ function draw() {
 
     }
 
+
+    steveWidth += .5;
+    steveHeight +=.5;
+     image(steve, width/2 - steveWidth/2, height/2 - steveHeight / 2, steveWidth, steveHeight);
+
+
+    fill(`rgba(${r}, 0,0, .25)`);
+
+    r += c;
+    rect(width/2 - steveWidth/2,height/2 - steveHeight / 2,steveWidth,steveHeight);
+    if (r > 255 || r < 1) {
+        c *= -1
+    }
+
+    fill('white');
     var paragraph = "When your professor tells a corny joke ";
     text(paragraph, 15, 50);
-    fill('white');
     textStyle(BOLD);
     text("But you need those 3 credits", 80, 550);
     fill('white');
     stroke(20);
     textSize(30);
     textFont('Tahoma');
-
-
 }
