5dc5638c82421a3c6d95f44ab8d994c247bbdbf0
lgraham93 <lgraham@github.com>
2018-10-18 15:03:20 -0700

    completed meme animation

--- meme_1/sketch.js
+++ meme_1/sketch.js
@@ -5,20 +5,22 @@
 
 //global
 var carlton;
-var simonc;
-var raw;
+var will;
 var crying;
-var steveWidth=578/4;
-var steveHeight=878/4;
+var hqdefault;
+var steveWidth = 578 / 4;
+var steveHeight = 878 / 4;
+var f; //framecount
 var r = 0;
 var c = 10;
 
 function preload() {
     carlton = loadImage("carlton.jpg");
-    raw = loadImage("raw.png");
-    crying=loadImage("crying.png");
-    guys=loadImage("guys.jpg");
-    steve=loadImage("steve.png");
+    will = loadImage("will.jpg");
+    crying = loadImage("crying.png");
+    guys = loadImage("guys.jpg");
+    steve = loadImage("steve.png");
+    hqdefault = loadImage("hqdefault.jpg");
 }
 
 function setup() {
@@ -38,39 +40,49 @@ function draw() {
 
     } else if (mouseX > width / 2 && mouseY < height / 2) {
         //quad 2
-        image(crying, 0, 0,width, height);
-    
-        
+        image(crying, 0, 0, width, height);
+
+
     } else if (mouseX < width / 2 && mouseY > height / 2) {
         // quad 3
-        image(raw, 0, 0,width, height);
+        image(will, 0, 0, width, height);
     } else {
         //quad 4
-        image(guys,  0, 0,width, height);
+        image(guys, 0, 0, width, height);
 
     }
 
 
     steveWidth += .5;
-    steveHeight +=.5;
-     image(steve, width/2 - steveWidth/2, height/2 - steveHeight / 2, steveWidth, steveHeight);
+    steveHeight += .5;
+    image(steve, width / 2 - steveWidth / 2, height / 2 - steveHeight / 2, steveWidth, steveHeight);
 
 
-    fill(`rgba(${r}, 0,0, .25)`);
+    //    fill(`rgba(${r}, 0,0, .25)`);
+    // alex we're not learning string literals!!
+    fill(r, 0, 0, 63);
 
     r += c;
-    rect(width/2 - steveWidth/2,height/2 - steveHeight / 2,steveWidth,steveHeight);
+    rect(width / 2 - steveWidth / 2, height / 2 - steveHeight / 2, steveWidth, steveHeight);
     if (r > 255 || r < 1) {
         c *= -1
     }
 
     fill('white');
-    var paragraph = "When your professor tells a corny joke ";
+    textSize(20);
+    var paragraph = "When you laughed at all the professors corny jokes ";
     text(paragraph, 15, 50);
     textStyle(BOLD);
-    text("But you need those 3 credits", 80, 550);
+    textSize(30);
+    text("And they still fail you", 80, 550);
     fill('white');
     stroke(20);
-    textSize(30);
     textFont('Tahoma');
+
+    translate(width / 2, height / 2);
+    var f = frameCount / 300 * PI;
+    shearX(f);
+
+    image(hqdefault, 60, 60);
+
 }
